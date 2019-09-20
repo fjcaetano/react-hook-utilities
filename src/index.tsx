32fe87ccdec97c6b3b7845f32abaeda5510b154a
Flávio Caetano <flavio@vieiracaetano.com>
2019-09-20 17:40:03 -0300

    fix(Types): use new 'readonly' syntax

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import {
-  DependencyList,
   useEffect,
   useLayoutEffect,
   useCallback,
@@ -11,30 +10,30 @@ import {
  * Executes an asynchronous effect
  *
  * @param effect the effect to be executed.
- * @param deps the effect's dependencies.
+ * @param dependencies the effect's dependencies.
  */
 export const useAsyncEffect = (
   effect: () => Promise<void>,
-  deps?: DependencyList,
+  dependencies?: readonly any[],
 ) => {
   useEffect(() => {
     effect();
-  }, deps);
+  }, dependencies);
 };
 
 /**
  * Executes a layout effect asynchronously.
  *
  * @param effect the layout effect to be executed.
- * @param deps the effect's dependencies.
+ * @param dependencies the effect's dependencies.
  */
 export const useAsyncLayoutEffect = (
   effect: () => Promise<void>,
-  deps?: DependencyList,
+  dependencies?: readonly any[],
 ) => {
   useLayoutEffect(() => {
     effect();
-  }, deps);
+  }, dependencies);
 };
 
 /**
@@ -43,11 +42,11 @@ export const useAsyncLayoutEffect = (
  * Errors thrown by the worker are caught and returned as a state.
  *
  * @param worker The callback function that runs a promise.
- * @param deps The callback dependencies.
+ * @param dependencies The callback dependencies.
  */
-export const useWorker = <TArgs extends any[], TRet>(
+export const useWorker = <TArgs extends readonly any[], TRet>(
   worker: (...args: TArgs) => Promise<TRet>,
-  deps: DependencyList,
+  dependencies: readonly any[],
 ): {
   isLoading: boolean;
   setIsLoading: (_: boolean) => void;
@@ -57,22 +56,21 @@ export const useWorker = <TArgs extends any[], TRet>(
 } => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | undefined>(undefined);
-  const callback = useCallback(
-    (async (...args: TArgs): Promise<TRet | undefined> => {
-      try {
-        setIsLoading(true);
-        const result = await worker(...args);
-        setIsLoading(false);
-        setError(undefined);
+  const callback = useCallback(async (...args: TArgs): Promise<
+    TRet | undefined
+  > => {
+    try {
+      setIsLoading(true);
+      const result = await worker(...args);
+      setIsLoading(false);
+      setError(undefined);
 
-        return result;
-      } catch (e) {
-        setIsLoading(false);
-        setError(e);
-      }
-    }) as (...args: any[]) => any,
-    deps,
-  );
+      return result;
+    } catch (e) {
+      setIsLoading(false);
+      setError(e);
+    }
+  }, dependencies);
 
   return { callback, error, isLoading, setError, setIsLoading };
 };
@@ -83,11 +81,11 @@ export const useWorker = <TArgs extends any[], TRet>(
  * @param effect The effect to be executed. Receives the dependencies' previous state as arguments.
  * @param dependencies The effect's dependencies.
  */
-export const useEffectUpdate = <Deps extends ReadonlyArray<any>>(
-  effect: (oldState: Deps) => void | (() => void),
-  dependencies: Deps,
+export const useEffectUpdate = <Dependencies extends readonly any[]>(
+  effect: (oldState: Dependencies) => void | (() => void),
+  dependencies: Dependencies,
 ) => {
-  const oldState = useRef<Deps>([] as any);
+  const oldState = useRef<Dependencies>([] as any);
   useEffect(() => {
     try {
       return effect(oldState.current);
@@ -105,10 +103,10 @@ export const useEffectUpdate = <Deps extends ReadonlyArray<any>>(
  * @param effect The effect callback to be executed.
  * @param dependencies The effect's dependencies.
  */
-export const useConditionalEffect = <Deps extends ReadonlyArray<any>>(
-  evalCondition: (oldState: Deps) => boolean,
+export const useConditionalEffect = <Dependencies extends readonly any[]>(
+  evalCondition: (oldState: Dependencies) => boolean,
   effect: () => (() => void) | void,
-  dependencies: Deps,
+  dependencies: Dependencies,
 ) => {
   useEffectUpdate(
     oldState => (evalCondition(oldState) ? effect() : undefined),
