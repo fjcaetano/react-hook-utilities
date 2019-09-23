3066af1c415357bc83dca0cc14dff1e84b576cbf
Flávio Caetano <flavio@vieiracaetano.com>
2019-09-23 00:14:06 -0300

    chore(CI): add workflow to release docs

---
+++ scripts/deploy-docs-folder.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+REPO_PATH="https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+git worktree add docs origin/gh-pages && \
+    yarn docs && \
+    cd docs && \
+    git add -f . && \
+    git config --global user.email "actions@github.com" && \
+    git config --global user.name "Github Actions" && \
+    git commit -m "Deploying docs ${GITHUB_SHA}" --quiet --no-verify && \
+    git push "$REPO_PATH" HEAD:gh-pages
+
+git worktree remove docs
